c271bbd7ca635cccd56b2ebca7c5649eaaddecfd
erdemir123 <sadikerdemir93@gmail.com>
2023-05-29 00:41:52 +0300

    board copmponets created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
 import "./App.css";
-import Dashboard from "./components/home/Dashboard";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <div className="App">
-    <Dashboard />
-  </div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
